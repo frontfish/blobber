2513fc071a7e2f3dd4f02cc1c1678cc6dc67c625
Christopher Hinstorff <chris@chinstorff.com>
2014-06-22 20:39:29 -0700

    add animation to end text

--- src/end.js
+++ src/end.js
@@ -20,15 +20,21 @@ Game.End.prototype = {
 
 	end.successText = game.add.text(Game.w / 2, 40, 'Success!', { font: '95px Arial', fill: '#ccc' });
 	end.successText.anchor.x = 0.5;
+	end.successText.alpha = 0;
+
 	end.playText = game.add.text(Game.w / 2, 200, 'Press UP to continue', { font: '33px Arial', fill: this.parseColor(Game.levels[localStorage.levelIndex].enemyColor || 'purple') });
 	end.playText.anchor.x = 0.5;
+	end.playText.alpha = 0;
+
 	end.menuText = game.add.text(Game.w / 2, 245, 'Press DOWN to return to menu', { font: '20px Arial', fill: '#ccc' });
 	end.menuText.anchor.x = 0.5;
+	end.menuText.alpha = 0;
 
 	end.scoreText = game.add.text(10, 10, Game.Play.prototype.calcScore() + ' / ' + play.level.scoreGoal, { font: '20px Arial', fill: '#aaa' });
 
 	end.attrText = game.add.text(Game.w - 3, Game.h - 3, 'music: "Ouroboros" by Kevin Macleod (incompetech.com)', { font: '10px Arial', fill: '#aaa' });
 	end.attrText.anchor.setTo(1, 1);
+	end.attrText.alpha = 0;
 
 	if (!play.levelWon) {
 	    end.successText.text = 'Failure!';
@@ -42,7 +48,9 @@ Game.End.prototype = {
 	    end.scoreText.fill = this.parseColor('purple');
 
 	    end.beatText = game.add.text(Game.w / 2, 135, 'You beat level ' + (+localStorage.levelIndex || Game.levels.length), { font: '20px Arial', fill: '#ccc' });
-	    end.beatText.anchor.x = 0.5
+	    end.beatText.anchor.x = 0.5;
+	    end.beatText.alpha = 0;
+	    game.add.tween(end.beatText).to({ alpha: 1 }, 500, null, true, 0, 0, false);
 
 	    if (+localStorage.levelIndex === 0 && localStorage.gameBeat === 'false') {
 		end.beatText.text = "Congratulations, you beat the game!";
@@ -50,6 +58,12 @@ Game.End.prototype = {
 		localStorage.gameBeat = true;
 	    }
 	}
+
+	// tweens
+	game.add.tween(end.successText).to({ alpha: 1 }, 500, null, true, 0, 0, false);
+	game.add.tween(end.playText).to({ alpha: 1 }, 250, null, true, 250, 0, false);
+	game.add.tween(end.menuText).to({ alpha: 1 }, 250, null, true, 500, 0, false);
+	game.add.tween(end.attrText).to({ alpha: 1 }, 500, null, true, 750, 0, false);
     },
 
     update: function () {
